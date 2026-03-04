5dc7721bddc59e05a19fbd48b7c6e7df7306dce6
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-03-04 16:57:03 +0900

    [level 0] Title: 문자열 돌리기, Time: 220.99 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        if (a != null && a.length() >= 1 && a.length() <= 10) {
+            for(int i=0;i<a.length();i++){
+                System.out.println(a.charAt(i));
+            }
+        }
+    }
+}
