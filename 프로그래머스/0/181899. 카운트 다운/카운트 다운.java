80dee27c1cf6aa2860cf542d6ce29b45dbee4122
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-03-15 18:41:18 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = {};
+        int size = start_num - end_num + 1;
+        answer = new int[size];
+        for (int i = 0; i < size; i++) {
+            answer[i] = start_num - i;
+        }
+        return answer;
+    }
+}
