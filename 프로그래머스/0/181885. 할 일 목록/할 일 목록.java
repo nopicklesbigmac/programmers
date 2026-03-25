9cbc9ba5106ee708d319753bf6131dddf78e0dbe
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-03-25 15:12:44 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        String[] answer = {};
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                list.add(todo_list[i]);
+            }
+        }
+        answer = list.toArray(new String[0]);
+        return answer;
+    }
+}
