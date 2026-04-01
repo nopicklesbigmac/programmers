9a93060d350329cb14647379fc210b9f5fa88b75
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-04-01 14:19:49 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.08 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString, String pat) {
+        String answer = "";
+        int lastIndex = myString.lastIndexOf(pat);
+        answer = myString.substring(0, lastIndex + pat.length());
+        return answer;
+    }
+}
