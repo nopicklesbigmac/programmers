dd317e017f3b34665863fcabbe97f09998a900ff
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-04-12 15:22:18 +0900

    [level 0] Title: 정수 찾기, Time: 2.51 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = 0;
+        answer = Arrays.stream(num_list).anyMatch(num -> num == n) ? 1 : 0;
+        return answer;
+    }
+}
