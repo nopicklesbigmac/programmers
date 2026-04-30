5ebf1620f740b74ee460538f9ac1e3fae1ab3dfd
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-04-30 22:14:11 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.52 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        
+        int n = numbers.length;
+        answer = numbers[n - 1] * numbers[n - 2];
+        return answer;
+    }
+}
