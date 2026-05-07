9ffa834f97a789ebe2f90b5ac727d795fad733ae
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-05-07 21:05:18 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        double sqrt = Math.sqrt(n);
+        if (sqrt == (int)sqrt) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
