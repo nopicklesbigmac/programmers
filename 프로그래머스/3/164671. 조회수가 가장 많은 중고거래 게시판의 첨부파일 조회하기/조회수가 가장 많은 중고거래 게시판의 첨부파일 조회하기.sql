042bedbf66d8bbc84975da349ebd0490cd5e3899
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-06-08 23:52:18 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,13 @@
+SELECT 
+    CONCAT('/home/grep/src/', F.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) AS FILE_PATH
+FROM 
+    USED_GOODS_FILE F
+WHERE 
+    F.BOARD_ID = (
+        SELECT BOARD_ID 
+        FROM USED_GOODS_BOARD 
+        ORDER BY VIEWS DESC 
+        LIMIT 1
+    )
+ORDER BY 
+    F.FILE_ID DESC;
