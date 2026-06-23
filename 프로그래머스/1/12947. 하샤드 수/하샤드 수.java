d166bb5969211efab52ee32ef803773f5ba1f71f
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-06-23 23:54:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        int originalX = x;
+        
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        answer = originalX % sum == 0;
+        
+        return answer;
+    }
+}
