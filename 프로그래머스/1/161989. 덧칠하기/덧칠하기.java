5d4476cb5c9e2e3f17b3e6c7338f64d1c88b31c7
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-07-05 20:27:21 +0900

    [level 1] Title: 덧칠하기, Time: 1.07 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int lastPainted = 0;
+        for (int s : section) {
+            if (s > lastPainted) {
+                answer++;
+                lastPainted = s + m - 1;
+            }
+        }
+        return answer;
+    }
+}
