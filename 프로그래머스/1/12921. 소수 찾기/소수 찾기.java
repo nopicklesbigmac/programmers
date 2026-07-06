e94006792848c5c9fbba7b0df316abcfeea5cae1
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-07-06 21:21:30 +0900

    [level 1] Title: 소수 찾기, Time: 11.29 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] isNotPrime = new boolean[n + 1];
+
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (!isNotPrime[i]) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
