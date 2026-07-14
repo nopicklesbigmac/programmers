d2b6800c45f77845225374113dc9952028768de6
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-07-14 22:32:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        answer = dp[n];
+        
+        return answer;
+    }
+}
