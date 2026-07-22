5d3f00b99b7296a3421e612a93cad23aaf961cdc
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-07-22 23:58:40 +0900

    [level 2] Title: 가장 큰 수, Time: 98.88 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumbers, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (strNumbers[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : strNumbers) {
+            sb.append(str);
+        }
+
+         answer = sb.toString();
+        return answer;
+    }
+}
