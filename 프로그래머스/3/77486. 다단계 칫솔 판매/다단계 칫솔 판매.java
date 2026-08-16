5560fc4440d3dd05b7124ed0a7791527acc1a8bc
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-08-16 22:54:56 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 11.38 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = {};
+        int n = enroll.length;
+        answer = new int[n];
+        java.util.Map<String, Integer> map = new java.util.HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(enroll[i], i);
+        }
+
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (referral[i].equals("-")) {
+                parent[i] = -1;
+            } else {
+                parent[i] = map.get(referral[i]);
+            }
+        }
+
+        for (int i = 0; i < seller.length; i++) {
+            int current = map.get(seller[i]);
+            int money = amount[i] * 100;
+
+            while (current != -1 && money > 0) {
+                int give = money / 10;
+                answer[current] += money - give;
+                money = give;
+                current = parent[current];
+            }
+        }
+
+        return answer;
+    }
+}
