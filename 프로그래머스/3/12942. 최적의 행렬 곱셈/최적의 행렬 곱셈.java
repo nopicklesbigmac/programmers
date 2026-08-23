50b4dbfd12519b7fcfba2f8201a4775cc4abc097
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-08-24 00:58:11 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 12.62 ms, Memory: 57.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] matrix_sizes) {
+        int answer = 0;
+        int n = matrix_sizes.length;
+        int[][] dp = new int[n][n];
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = Integer.MAX_VALUE;
+
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k]
+                            + dp[k + 1][j]
+                            + matrix_sizes[i][0]
+                            * matrix_sizes[k][1]
+                            * matrix_sizes[j][1];
+
+                    dp[i][j] = Math.min(dp[i][j], cost);
+                }
+            }
+        }
+
+        answer = dp[0][n - 1];
+        return answer;
+    }
+}
