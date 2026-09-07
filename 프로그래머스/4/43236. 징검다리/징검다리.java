3b087fdba8b3ec43247e561de2b565da2d9098e5
nopicklesbigmac <110544246+nopicklesbigmac@users.noreply.github.com>
2026-09-07 17:24:58 +0900

    [level 4] Title: 징검다리, Time: 28.74 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+
+        Arrays.sort(rocks);
+
+        int left = 1;
+        int right = distance;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int removed = 0;
+            int prev = 0;
+
+            for (int rock : rocks) {
+                if (rock - prev < mid) {
+                    removed++;
+                } else {
+                    prev = rock;
+                }
+            }
+
+            if (distance - prev < mid) {
+                removed++;
+            }
+
+            if (removed <= n) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+}
